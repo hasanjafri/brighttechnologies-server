81fdd33726c6c432fc5f1250993cd3dc055f19de
Hasan Jafri <hasan.jafri7@gmail.com>
2019-03-20 22:00:56 -0400

    Added models and db file, installed sqlalchemy

---
+++ src/app/database.py
@@ -0,0 +1,22 @@
+import os
+from contextlib import contextmanager
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine(os.environ['DATABASE_URL'])
+
+# Session to be used throughout app.
+Session = sessionmaker(bind=engine)
+
+@contextmanager
+def scoped_session():
+    session = Session()
+    try:
+        yield session
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
---
+++ src/app/models/job.py
@@ -0,0 +1,9 @@
+""" Module represents a Job Posting. """
+
+import datetime
+
+from sqlalchemy import (
+    Column, String, DateTime
+)
+
+from app.models import Base
